75317e3b8def85833f803a69345d48789cdae09d
Wajar Novianto <wajar04@gmail.com>
2021-10-19 11:17:21 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/ndaki/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ndaki
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = 'user@gmail.com';
+  String password = '123456';
+
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+  final formKey = new GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(50),
+                child: Image.asset(
+                  'assets/keyboard.jpg',
+                  scale: 3,
+                ),
+              ),
+              _formBuilder(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _formBuilder() {
+    return Form(
+      key: formKey,
+      // autovalidate: true,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.white),
+              validator: (value) {
+                return value!.isEmpty ? "Email Tidak Boleh Kosong" : null;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink, width: 10)),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1.5)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1.5)),
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              validator: (value) {
+                return value!.isEmpty ? "Password Tidak Boleh Kosong" : null;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white)),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 1.5),
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 1.5)),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: FlatButton(
+              onPressed: () {},
+              child: Text(
+                'Lupa Password ?',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400),
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              if (formKey.currentState!.validate() &&
+                  emailController.text.toString() == email) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MyHomePage(
+                              title: 'NdakiYuk',
+                            )),
+                    (Route<dynamic> route) => false);
+              }
+            },
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.07,
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 24, color: Colors.lightBlue),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: ElevatedButton(
+              child: Text(
+                'Buat akun',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
